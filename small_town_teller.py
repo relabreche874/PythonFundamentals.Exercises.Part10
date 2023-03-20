dc4fba3fd20a9f6ed759519ba3b61407760f75e6
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-20 18:14:04 -0400

    Updated small_town_teller code

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,4 +1,6 @@
 class Person:  # Person class for defining customer
+    last_name: object
+
     def __init__(self, id, first_name, last_name):
         self.id = id  # Customer ID
         self.first_name = first_name  # Customer first name
@@ -6,35 +8,38 @@ class Person:  # Person class for defining customer
 
 
 class Account:  # Account class for defining bank account
-    def __init__(self, number, type, owner):  # Define init method for Account class, self refers to class instance
+    def __init__(self, number, ac_type, owner):  # Define init method for Account class, self refers to class instance
         self.number = number  # The account number
-        self.type = type  # The type of account
+        self.ac_type = ac_type  # The type of account
         self.owner = owner  # Owner of the account
         self.balance = 100  # Balance of the account
 
 
 class Bank:
     def __init__(self):
-        self.customers = []
-        self.accounts = []
+        self.customers = {}
+        self.accounts = {}
 
-    def add_customer(self, customer):
-        self.customers.append(customer)
+    def add_customer(self, customer: Person):
+        self.customers[customer.id] = customer.first_name + " " + customer.last_name
 
-    def add_account(self, account):
-        self.accounts.append(account)
+    def add_account(self, account: Account):
+        self.accounts[account.number] = account
 
-    def rem_account(self, account):
-        self.accounts.remove(account)
+    def rem_account(self, number):
+        del self.accounts[number]
 
-    def deposit(self, account, amount):
-        self.accounts.balance += int(amount)
+    def deposit(self, number, amount):
+        if number in self.accounts:
+            dep_account = self.accounts.get(number)
+            dep_account.balance += float(amount)
 
-    def withdraw(self, account, amount):
-        if int(account) >= int(amount):
-            self.accounts.balance - amount
+    def withdrawal(self, number, amount):
+        if number in self.accounts:  #and amount < self.accounts.balance:
+            with_account = self.accounts.get(number)
+            with_account.balance -= float(amount)
         else:
             return "You do not have sufficient funds"
 
-    def balance_inquiry(self, account):
-        return self.accounts.balance
+    def balance_inquiry(self, number):
+        return self.accounts.get(number).balance
